db918b578c5837c4213e5b8af425b45d522806bb
Josjay05 <ojoshjay@gmail.com>
2024-07-09 11:37:22 +0100

    commit

--- src/components/CartPage.tsx
+++ src/components/CartPage.tsx
@@ -33,20 +33,22 @@
 // export default CartPage;
 
 
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { RootState } from '../constant/Store';
-import { removeFromCart, updateQuantity } from '../constant/CartSlice';
+import {  updateQuantity } from '../constant/CartSlice';
+import { BiMinus, BiPlus } from 'react-icons/bi';
 
 const CartPage: React.FC = () => {
   const cart = useSelector((state: RootState) => state.cart.items);
   const dispatch = useDispatch();
-
-  const handleRemoveFromCart = (productId: number, size: string) => {
-    dispatch(removeFromCart({ id: productId, size }));
-  };
-
+  const [size, setSize] = useState('S');
+//   const [quantity, setQuantity] = useState(1);
+//   const handleRemoveFromCart = (productId: number, size: string) => {
+//     dispatch(removeFromCart({ id: productId, size }));
+//   };
+ 
   const handleUpdateQuantity = (productId: number, size: string, quantity: number) => {
     dispatch(updateQuantity({ id: productId, size, quantity }));
   };
@@ -56,27 +58,54 @@ const CartPage: React.FC = () => {
   }
 
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold">Cart</h1>
+    <>
+      <h1 className="text-3xl font-bold my-[50px] ml-7 m">Cart</h1>
+    <section className="lg:p-4 p-2 lg:grid lg:grid-cols-2 gap-4">
       {cart.map(product => (
-        <div key={`${product.id}-${product.size}`} className="flex justify-between items-center p-4 bg-gray-100 mb-2">
-          <img src={product.imageUrl} alt={product.title} className="w-20 h-20 object-cover" />
-          <div className="flex-grow ml-4">
-            <h3 className="font-semibold">{product.title}</h3>
-            <p>Size: {product.size}</p>
+        <article key={`${product.id}-${product.size}`} className="flex md:grid md:grid-cols-2 md:gap-12 lg:grid lg:grid-cols-2 items-center md: lg:p-4 md:p-[34px] p-2 border border-gray mb-2 gap-1">
+         
+         <article className='ls '>
+      <img src={product.imageUrl} alt={product.title} className="w-full  sm:max-h-[150px] sm:w-[300px] md:max-h-[350px]   lg:h-[400px]" />
+      <div className='bg-[#F5FF32] lg:py-3 md:p-3 px-1'>
+      <h1 className="lg:text-2xl font-bold py-2">{product.title}</h1>
+      <p className="text-xl font-semibold">${product.price.toLocaleString()}</p>
+      </div>
+      
+      </article>
+          <div className="flex flex-col gap-8 sm:gap-3">
             <div>
-              <label>Quantity:</label>
-              <button onClick={() => handleUpdateQuantity(product.id, product.size, product.quantity > 1 ? product.quantity - 1 : 1)}>-</button>
+            <div className='flex lg:gap-[37px] md:gap-6 py-2'>
+        <label>Size:</label>
+        <select value={size} onChange={(e) => setSize(e.target.value)}>
+          <option value="S">S</option>
+          <option value="M">M</option>
+          <option value="L">L</option>
+          <option value="XL">XL</option>
+        </select>
+      </div>
+      
+      
+      <div className='flex items-center lg:gap-3 gap-[3px] md:gap-2 my-4 sm:my-1'>
+      <label>Quantity:</label>
+              <button  className='lg:text-[22px] bg-black  text-white font-light' onClick={() => handleUpdateQuantity(product.id, product.size, product.quantity > 1 ? product.quantity - 1 : 1)}><BiMinus/></button>
               <span>{product.quantity}</span>
-              <button onClick={() => handleUpdateQuantity(product.id, product.size, product.quantity + 1)}>+</button>
+              <button  className='lg:text-[22px] bg-black  text-white font-light' onClick={() => handleUpdateQuantity(product.id, product.size, product.quantity + 1)}><BiPlus/></button>
             </div>
+      </div>
+              
             <p>${(product.price * product.quantity).toLocaleString()}</p>
           </div>
-          <button onClick={() => handleRemoveFromCart(product.id, product.size)} className="bg-red-500 text-white py-2 px-4 rounded">Remove</button>
-        </div>
+         
+        </article>
       ))}
-      <Link to="/checkout" className="bg-green text-white py-2 px-4 rounded">Proceed to Checkout</Link>
-    </div>
+      
+    </section>
+      <div className="flex md:flex-col sm:flex-col justify-center items-center my-6 lg:gap-20 gap-6">
+      <Link to="/checkout" className="bg-black text-white py-2 px-4 rounded">Proceed to Checkout</Link>
+
+      <Link to="/" className="bg-black text-white py-2 px-4 rounded ml-4">Continue Shopping</Link>
+      </div>
+    </>
   );
 };
 
--- src/components/SingleProduct.tsx
+++ src/components/SingleProduct.tsx
@@ -42,6 +42,7 @@ import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../constant/Store';
 import { addToCart } from '../constant/CartSlice';
+import { BiMinus, BiPlus } from 'react-icons/bi';
 
 const ProductDetails: React.FC = () => {
   const { id } = useParams<{ id: string }>();
@@ -65,29 +66,41 @@ const ProductDetails: React.FC = () => {
   };
 
   return (
-    <div className="p-4">
-      <img src={product.imageUrl} alt={product.title} className="w-full h-auto" />
-      <h1 className="text-2xl font-bold">{product.title}</h1>
+    <section className="p-4 flex items-center gap-[2rem] my-20">
+             
+      <article>
+      <img src={product.imageUrl} alt={product.title} className="w-full h-auto   lg:h-auto" />
+      <div className='bg-[#F5FF32] py-3 px-1'>
+      <h1 className="text-2xl font-bold py-2">{product.title}</h1>
       <p className="text-xl font-semibold">${product.price.toLocaleString()}</p>
-      <div>
+      </div>
+      
+      </article>
+        <article>
+      <div className='flex gap-[37px] py-2'>
         <label>Size:</label>
         <select value={size} onChange={(e) => setSize(e.target.value)}>
-          <option value="S">Small</option>
-          <option value="M">Medium</option>
-          <option value="L">Large</option>
-          <option value="XL">Extra Large</option>
+          <option value="S">S</option>
+          <option value="M">M</option>
+          <option value="L">L</option>
+          <option value="XL">XL</option>
         </select>
       </div>
-      <div>
+      <div className='flex  items-center gap-3'>
         <label>Quantity:</label>
-        <button onClick={() => setQuantity(quantity > 1 ? quantity - 1 : 1)}>-</button>
+        <button className='text-[22px] bg-black  text-white font-light ' onClick={() => setQuantity(quantity > 1 ? quantity - 1 : 1)}> <BiMinus/></button>
         <span>{quantity}</span>
-        <button onClick={() => setQuantity(quantity + 1)}>+</button>
+        <div className='flex items-start gap-2'>
+        <button onClick={() => setQuantity(quantity + 1)} className='text-[22px] bg-black  text-white font-light bg'><BiPlus/></button>
+        </div>
       </div>
       <div>
-        <button onClick={handleAddToCart} className="bg-blue text-white py-2 px-4 rounded">Add to Cart</button>
+        <button onClick={handleAddToCart} className="my-5 bg-black text-white py-[3px] px-3 rounded-xl cursor-pointer">Add to Cart</button>
       </div>
-    </div>
+        </article>
+
+      
+    </section>
   );
 };
 
